54f1f33058aab932e32d78f58e71ac8da12afb79
srinaath21 <81795799+srinaath21@users.noreply.github.com>
2021-09-24 22:49:16 +0530

    Delete multiplication_table.py

--- multiplication_table.py
+++
@@ -1,5 +0,0 @@
-number = int(input("Enter a number : "))
-num_range = int(input("Enter the range of table : "))
-for count in range(1, num_range + 1 ):
-    product = count * number
-    print(number,"x",count, "=", product)
